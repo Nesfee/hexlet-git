4a1056efb3eec258378cba29717a7920a2130075
Kiryl <Nesfee1@mail.ru>
2022-04-06 15:55:48 +0200

    fix main.py

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-print("Hello!!!")
-#do evrything for yourself!!!
+def binary_sum(num1, num2):
+    if num1 < num2:
+        while num1 < num2:
+            num1 += 1
+    elif num1 > num2:
+        while num1 > num2:
+            num2 += 1
+    else:
+        return num1, num2
+    return num1, num2
+
+
+if __name__ == "__main__":
+    print(binary_sum(2, 30))
+    
+
 
